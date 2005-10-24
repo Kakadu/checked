d64ffafbb81355648aa71f2d3a6877ddbe63d329
db <db@454aa00d-cbff-0310-9ee6-ad4123b06f8f>
2005-10-24 14:22:15 +0000

    Reimplemented test using REPR
    git-svn-id: https://oops.math.spbu.ru/svn/checked/trunk@10 454aa00d-cbff-0310-9ee6-ad4123b06f8f

--- regression/test001.ml
+++ regression/test001.ml
@@ -6,51 +6,64 @@ let _ =
     let buffer = Buffer.create 1024 in
     let module IntCheck = Checked.Viewer (View.Integer)(View.String) in
     Buffer.add_string buffer "Testing map function:\n"; 
-    let _ = REPR (1) in
+
     let s, v = REPR (map (fun x -> x+1) (Ok 2)) in
     Buffer.add_string buffer (sprintf "%s = %s\n" s (IntCheck.toString v));
 
-             (* (IntCheck.toString (map (fun x -> x+1) (Ok 2))));*)
-    Buffer.add_string buffer (sprintf "map (fun x -> x+1) (Fail []) = %s\n" 
-          (IntCheck.toString (map (fun x -> x+1) (Fail []))));
+    let s, v = REPR (map (fun x -> x+1) (Fail [])) in
+    Buffer.add_string buffer (sprintf "%s = %s\n" s (IntCheck.toString v));
 
     Buffer.add_string buffer "Testing (@>) operation:\n";
-    Buffer.add_string buffer (sprintf "(fun x -> x+1) @> (Ok 2) = %s\n" 
-          (IntCheck.toString ((fun x -> x+1) @> (Ok 2))));
-    Buffer.add_string buffer (sprintf "(fun x -> x+1) @> (Fail []) = %s\n" 
-          (IntCheck.toString ((fun x -> x+1) @> (Fail []))));
+
+    let s, v = REPR ((fun x -> x+1) @> (Ok 2)) in
+    Buffer.add_string buffer (sprintf "%s = %s\n" s (IntCheck.toString v));
+
+    let s, v = REPR ((fun x -> x+1) @> (Fail [])) in
+    Buffer.add_string buffer (sprintf "%s = %s\n" s (IntCheck.toString v));
 
     Buffer.add_string buffer "Testing join function:\n";
-    Buffer.add_string buffer (sprintf "join (fun x -> Ok (x+1)) (Ok 2) = %s\n" 
-          (IntCheck.toString (join (fun x -> Ok (x+1)) (Ok 2))));
-    Buffer.add_string buffer (sprintf "join (fun x -> Ok (x+1)) (Fail []) = %s\n" 
-          (IntCheck.toString (join (fun x -> Ok (x+1)) (Fail []))));
-    Buffer.add_string buffer (sprintf "join (fun x -> Fail[]) (Ok 2) = %s\n" 
-          (IntCheck.toString (join (fun x -> Fail[]) (Ok 2))));
-    Buffer.add_string buffer (sprintf "join (fun x -> Fail[]) (Fail []) = %s\n" 
-          (IntCheck.toString (join (fun x -> Fail[]) (Fail []))));
+
+    let s, v = REPR (join (fun x -> Ok (x+1)) (Ok 2)) in
+    Buffer.add_string buffer (sprintf "%s = %s\n" s (IntCheck.toString v));
+
+    let s, v = REPR (join (fun x -> Ok (x+1)) (Fail [])) in
+    Buffer.add_string buffer (sprintf "%s = %s\n" s (IntCheck.toString v));
+
+    let s, v = REPR (join (fun x -> Fail[]) (Ok 2)) in
+    Buffer.add_string buffer (sprintf "%s = %s\n" s (IntCheck.toString v));
+
+    let s, v = REPR (join (fun x -> Fail[]) (Fail [])) in
+    Buffer.add_string buffer (sprintf "%s = %s\n" s (IntCheck.toString v));
 
     Buffer.add_string buffer "Testing (@@>) function:\n";
-    Buffer.add_string buffer (sprintf "(fun x -> Ok (x+1)) @@> (Ok 2) = %s\n" 
-          (IntCheck.toString ((fun x -> Ok (x+1)) @@> (Ok 2))));
-    Buffer.add_string buffer (sprintf "(fun x -> Ok (x+1)) @@> (Fail []) = %s\n" 
-          (IntCheck.toString ((fun x -> Ok (x+1)) @@> (Fail []))));
-    Buffer.add_string buffer (sprintf "(fun x -> Fail[]) @@> (Ok 2) = %s\n" 
-          (IntCheck.toString ((fun x -> Fail[]) @@> (Ok 2))));
-    Buffer.add_string buffer (sprintf "(fun x -> Fail[]) @@> (Fail []) = %s\n" 
-          (IntCheck.toString ((fun x -> Fail[]) @@> (Fail []))));
+
+    let s, v = REPR ((fun x -> Ok (x+1)) @@> (Ok 2)) in
+    Buffer.add_string buffer (sprintf "%s = %s\n" s (IntCheck.toString v));
+
+    let s, v = REPR ((fun x -> Ok (x+1)) @@> (Fail [])) in
+    Buffer.add_string buffer (sprintf "%s = %s\n" s (IntCheck.toString v));
+
+    let s, v = REPR ((fun x -> Fail[]) @@> (Ok 2)) in
+    Buffer.add_string buffer (sprintf "%s = %s\n" s (IntCheck.toString v));
+
+    let s, v = REPR ((fun x -> Fail[]) @@> (Fail [])) in
+    Buffer.add_string buffer (sprintf "%s = %s\n" s (IntCheck.toString v));
 
     Buffer.add_string buffer "Chain @> test:\n";
-    Buffer.add_string buffer (sprintf "(fun x -> x+1) @> (fun x -> x+2) @> (Ok 3) = %s\n" 
-          (IntCheck.toString ((fun x -> x+1) @> (fun x -> x+2) @> (Ok 3))));
-    Buffer.add_string buffer (sprintf "(fun x -> x+1) @> (fun x -> x+2) @> (Fail []) = %s\n" 
-          (IntCheck.toString ((fun x -> x+1) @> (fun x -> x+2) @> (Fail []))));
+
+    let s, v = REPR ((fun x -> x+1) @> (fun x -> x+2) @> (Ok 3)) in
+    Buffer.add_string buffer (sprintf "%s = %s\n" s (IntCheck.toString v));
+
+    let s, v = REPR ((fun x -> x+1) @> (fun x -> x+2) @> (Fail [])) in
+    Buffer.add_string buffer (sprintf "%s = %s\n" s (IntCheck.toString v));
 
     Buffer.add_string buffer "Chain @>/@@> test:\n";
-    Buffer.add_string buffer (sprintf "(fun x -> x+1) @> (fun x -> Ok (x+2)) @>> (Ok 3) = %s\n" 
-          (IntCheck.toString ((fun x -> x+1) @> (fun x -> Ok (x+2)) @@> (Ok 3))));
-    Buffer.add_string buffer (sprintf "(fun x -> x+1) @> (fun x -> Ok (x+2)) @>> (Fail []) = %s\n" 
-          (IntCheck.toString ((fun x -> x+1) @> (fun x -> Ok (x+2)) @@> (Fail []))));
+
+    let s, v = REPR ((fun x -> x+1) @> (fun x -> Ok (x+2)) @@> (Ok 3)) in
+    Buffer.add_string buffer (sprintf "%s = %s\n" s (IntCheck.toString v));
+
+    let s, v = REPR ((fun x -> x+1) @> (fun x -> Ok (x+2)) @@> (Fail [])) in
+    Buffer.add_string buffer (sprintf "%s = %s\n" s (IntCheck.toString v));
 
     let module IntListCheck = Checked.Viewer 
        (
@@ -63,16 +76,17 @@ let _ =
          end
        ) (View.String)
     in
+
     Buffer.add_string buffer "Testing mapList function:\n";
-    Buffer.add_string buffer (sprintf "mapList [Ok 1; Ok 2; Ok 3] = %s\n"
-          (IntListCheck.toString (mapList [Ok 1; Ok 2; Ok 3]))); 
-    Buffer.add_string buffer (sprintf "mapList [Ok 1; Fail []; Ok 3] = %s\n"
-          (IntListCheck.toString (mapList [Ok 1; Fail []; Ok 3]))); 
-    Buffer.add_string buffer 
-          (sprintf "mapList [Ok 1; Fail [\"t1\"]; Ok 3; Fail [\"t1\"]] = %s\n"
-             (IntListCheck.toString 
-                 (mapList [Ok 1; Fail ["t1"]; Ok 3; Fail ["t2"]])
-          )); 
+
+    let s, v = REPR (mapList [Ok 1; Ok 2; Ok 3]) in
+    Buffer.add_string buffer (sprintf "%s = %s\n" s (IntListCheck.toString v)); 
+
+    let s, v = REPR (mapList [Ok 1; Fail []; Ok 3]) in
+    Buffer.add_string buffer (sprintf "%s = %s\n" s (IntListCheck.toString v)); 
+
+    let s, v = REPR (mapList [Ok 1; Fail ["t1"]; Ok 3; Fail ["t2"]]) in
+    Buffer.add_string buffer (sprintf "%s = %s\n" s (IntListCheck.toString v)); 
 
     let module IntArrayCheck = Checked.Viewer 
        (
@@ -87,12 +101,14 @@ let _ =
     in
 
     Buffer.add_string buffer "Testing mapArray function:\n";
-    Buffer.add_string buffer (sprintf "mapArray [|Ok 1; Ok 2; Ok 3|] = %s\n"
-          (IntArrayCheck.toString (mapArray [|Ok 1; Ok 2; Ok 3|]))); 
-    Buffer.add_string buffer (sprintf "mapArray [|Ok 1; Fail []; Ok 3|] = %s\n"
-          (IntArrayCheck.toString (mapArray [|Ok 1; Fail []; Ok 3|])));
-    Buffer.add_string buffer 
-          (sprintf "mapArray [|Ok 1; Fail [\"t1\"]; Ok 3; Fail [\"t2\"]|] = %s\n"
-          (IntArrayCheck.toString (mapArray [|Ok 1; Fail ["t1"]; Ok 3; Fail ["t2"]|])));
 
-    printf "%s" (Buffer.contents buffer)
+    let s, v = REPR (mapArray [|Ok 1; Ok 2; Ok 3|]) in
+    Buffer.add_string buffer (sprintf "%s = %s\n" s (IntArrayCheck.toString v)); 
+
+    let s, v = REPR (mapArray [|Ok 1; Fail []; Ok 3|]) in
+    Buffer.add_string buffer (sprintf "%s = %s\n" s (IntArrayCheck.toString v));
+
+    let s, v = REPR (mapArray [|Ok 1; Fail ["t1"]; Ok 3; Fail ["t2"]|]) in
+    Buffer.add_string buffer (sprintf "%s = %s\n" s (IntArrayCheck.toString v));
+
+    printf "%s" (Buffer.contents buffer) 
