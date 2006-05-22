47f8c53da6a70bacf03de515011678a91e3dc311
db <db@454aa00d-cbff-0310-9ee6-ad4123b06f8f>
2006-05-22 10:08:08 +0000

    Docs brushed up
    git-svn-id: https://oops.math.spbu.ru/svn/checked/trunk@21 454aa00d-cbff-0310-9ee6-ad4123b06f8f

--- camlp4/pa_checked.ml
+++ camlp4/pa_checked.ml
@@ -17,8 +17,35 @@
 
 (** Pa_checked --- a camlp4 syntax extension to work with Checked exception
     monad. 
-*)
 
+    A new [ensure] construct is added at expression level. The construct 
+    allows to bind several checked values in a row. 
+
+    Example:
+
+    [ensure x, y, z in Ok (x+y+z) end]
+
+    is equivalent to
+
+    [x -?->> (fun x -> y -?->> (fun y -> z -?->> (fun z -> Ok (x+y+z))))]
+
+    Note that the body of [ensure] expression has to be of checked type and that
+    the [ensure] key values have to be simple identifiers.
+
+    Another flavour of [ensure] construct is introduced to provide
+    pattern-matching facility. 
+
+    Example:
+
+    [ensure (x as Some x), (y as Some y) in Ok (x+y) end]
+
+    The surrounding brackets are mandatory for [ensure] key values to distinguish the 
+    latter variant of the construct from the former.
+
+    The construct above is equivalent to
+
+    [x -?->> (fun (Some x) -> y -?->> (fun (Some y) -> Ok (x+y)))]
+*)
 
 (**/**)
 
